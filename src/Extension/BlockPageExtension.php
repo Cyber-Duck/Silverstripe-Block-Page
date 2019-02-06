368118be4a8115d6faae956195c5ee9634ad9b0a
Andrew Mc Cormack <andrewm@cyber-duck.co.uk>
2019-02-06 11:48:49 +0000

    remove custom delete action and orderable rows classes

--- src/Extension/BlockPageExtension.php
+++ src/Extension/BlockPageExtension.php
@@ -2,28 +2,21 @@
 
 namespace CyberDuck\BlockPage\Extension;
 
-use Page;
 use CyberDuck\BlockPage\Action\GridFieldVersionedContentBlockItemRequest;
-use CyberDuck\BlockPage\Action\GridFieldVersionedDeleteAction;
-use CyberDuck\BlockPage\Action\GridFieldVersionedOrderableRows;
 use CyberDuck\BlockPage\Model\ContentBlock;
-use CyberDuck\BlockPage\Model\PageContentBlock;
 use SilverStripe\Control\Controller;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
-use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 use SilverStripe\Forms\GridField\GridFieldDetailForm;
 use SilverStripe\Forms\GridField\GridFieldPageCount;
 use SilverStripe\Forms\GridField\GridFieldPaginator;
-use SilverStripe\Forms\GridField\GridFieldVersionedState;
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\ORM\DataObject;
 use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 class BlockPageExtension extends DataExtension
-{    
+{
     private static $db = [];
 
     private static $many_many = [
@@ -40,18 +33,15 @@ class BlockPageExtension extends DataExtension
         'ContentBlocks'
     ];
     
-    public function updateCMSFields(FieldList $fields) 
-    {   
-        if($this->owner->ID > 0) {
+    public function updateCMSFields(FieldList $fields)
+    {
+        if ($this->owner->ID > 0) {
             $editor = GridFieldConfig_RelationEditor::create();
             $grid = new GridField('ContentBlocks', 'Content Blocks', $this->owner->ContentBlocks(), $editor);
             $grid->getConfig()
-                ->removeComponentsByType(GridFieldDeleteAction::class)
                 ->removeComponentsByType(GridFieldPageCount::class)
                 ->removeComponentsByType(GridFieldPaginator::class)
-                ->addComponent(new GridFieldVersionedState(['Title']))
-                ->addComponent(new GridFieldVersionedOrderableRows('SortBlock'))
-                ->addComponent(new GridFieldVersionedDeleteAction(true))
+                ->addComponent(new GridFieldOrderableRows('SortBlock'))
                 ->getComponentByType(GridFieldDetailForm::class)
                 ->setItemRequestClass(GridFieldVersionedContentBlockItemRequest::class);
     
