c2f82032472e6d94fcc35f88d0607ea56708f498
Sam Jarvis <sam@samjarvis.co.uk>
2023-01-13 12:33:51 +0000

    i18n - replace $title property with i18n_singular_name, allow extending searchable fields

--- src/Model/ContentBlock.php
+++ src/Model/ContentBlock.php
@@ -4,7 +4,9 @@ namespace CyberDuck\BlockPage\Model;
 
 use Page;
 use SilverStripe\Control\Controller;
+use SilverStripe\Core\ClassInfo;
 use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Forms\DropdownField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\GridField\GridField;
@@ -25,193 +27,218 @@ use SilverStripe\Versioned\VersionedGridFieldItemRequest;
 
 class ContentBlock extends DataObject implements PermissionProvider
 {
-    private static $table_name = 'ContentBlock';
+	private static $table_name = 'ContentBlock';
 
-    private static $db = [];
+	private static $db = [];
 
-    private static $belongs_many_many = [
-        'Pages' => Page::class,
-    ];
+	private static $belongs_many_many = [
+		'Pages' => Page::class,
+	];
 
-    private static $owned_by = [
-        'Pages'
-    ];
+	private static $owned_by = [
+		'Pages'
+	];
 
-    private static $extensions = [
-        Versioned::class
-    ];
+	private static $extensions = [
+		Versioned::class
+	];
 
-    private static $versioned_gridfield_extensions = true;
-    
-    private static $singular_name = 'Content Block';
-    
-    private static $plural_name = 'Content Blocks';
+	private static $versioned_gridfield_extensions = true;
 
-    private static $summary_fields = [
-        'Thumbnail'   => '',
-        'ID'          => 'ID',
-        'ClassName'   => 'Content type',
-        'Title'       => 'Title',
-        'Pages.Count' => 'Pages'
-    ];
+	private static $singular_name = 'Content Block';
 
-    public function searchableFields()
-    {
-        return [
-            'ClassName' => [
-                'filter' => 'ExactMatchFilter',
-                'title' => 'Content Type',
-                'field' => DropdownField::create('ClassName')->setSource(ContentBlock::get()->map('ClassName', 'ClassName'))->setEmptyString('-- Content Type --')
-            ]
-        ];
-    }
+	private static $plural_name = 'Content Blocks';
 
-    /**
-     * @return DBField
-     */
-    public function getThumbnail()
-    {
-        return DBField::create_field('HTMLText', sprintf('<img src="%s" height="20">', $this->getPreviewImagePath()));
-    }
+	private static $summary_fields = [
+		'Thumbnail' => '',
+		'i18n_singular_name' => 'Content type',
+		'ID' => 'ID',
+		'Title' => 'Title',
+		'Pages.Count' => 'Pages'
+	];
 
-    /**
-     * @return string
-     */
-    protected function getPreviewImagePath()
-    {
-        $previewImagePath = $this->config()->get('preview');
+	private static $searchable_fields = [
+		'ID',
+		'ClassName'
+	];
 
-        $this->extend('updatePreviewImagePath', $previewImagePath);
+	public function searchableFields()
+	{
+		$fields = parent::searchableFields();
 
-        return $previewImagePath;
-    }
+		$block_types = ClassInfo::subclassesFor(ContentBlock::class);
 
-    public function getCMSFields()
-    {
-        $fields = parent::getCMSFields();
-        $fields->removeByName('Pages');
+		$block_options = [];
 
-        if($this->getAction() == 'new') {
-            return $this->getCMSSelectionFields($fields);
-        } else {
-            $editor = GridFieldConfig_RelationEditor::create();
-            $grid = new GridField('Pages', 'Pages', $this->Pages(), $editor);
-            $grid->getConfig()
-                ->removeComponentsByType(GridFieldAddNewButton::class);
-            $fields->addFieldToTab('Root.Pages', $grid);
-        }
-        return $fields;
-    }
-    
-    public function getTemplateHolder()
-    {
-        return $this->renderWith(['Block/ContentBlock_holder']);
-    }
-    
-    public function getTemplate()
-    {   
-        if($this->ClassName != ContentBlock::class) {
-            return $this->renderWith('Block/'.$this->ClassName);
-        }
-    }
-    
-    public function getAction()
-    {
-        $path = explode('/', Controller::curr()->getRequest()->getURL());
-        return array_pop($path);
-    }
+		foreach($block_types as $block_type) {
+			$block_inst = Injector::inst()->create($block_type);
+			$block_options[$block_type] = $block_inst->i18n_singular_name();
+		}
 
-    public function getBlockType()
-    {
-        $className = $this->owner->ClassName;
-        return $className::config()->get('title');
-    }
-    
-    private function getCMSSelectionFields(FieldList $fields)
-    {
-        $fields->removeByName('Root');
-        // fields used in the inital selection request
-        $session = Controller::curr()->getRequest()->getSession();
-        $fields->push(HiddenField::create('BlockRelationID')->setValue($session->get('BlockRelationID')));
-        $fields->push(HiddenField::create('BlockRelationClass')->setValue($session->get('BlockRelationClass')));
+		asort($block_options);
 
-        // create the selection tab and options
-        $fields->push(TabSet::create('Root', Tab::create('Main')));
+		$fields['ID'] = [
+			'filter' => 'ExactMatchFilter',
+			'title' => 'ID'
+		];
 
-        $rules = (array) Config::inst()->get(ContentBlock::class, 'restrict');
-        
-        if(array_key_exists($session->get('BlockRelationClass'), $rules)) {
-            $classes = $rules[$session->get('BlockRelationClass')];
-        } else {
-            $classes = (array) Config::inst()->get(ContentBlock::class, 'blocks');
-        }
-        $options = [];
-        foreach($classes as $class) {
-            $options[$class] = DBField::create_field('HTMLText', Controller::curr()
-                ->customise([
-                    'Preview'     => singleton($class)->getPreviewImagePath(),
-                    'Title'       => $class::config()->get('title'),
-                    'Description' => $class::config()->get('description')
-                ])
-                ->renderWith('/Includes/ContentBlockOption')
-            );
-        }
-        $checked = key(array_slice($options, 0, 1, true));
+		$fields['ClassName'] = [
+			'filter' => 'ExactMatchFilter',
+			'title' => 'Content type',
+			'field' => DropdownField::create('ClassName')->setSource($block_options)->setEmptyString('- Any -')
+		];
 
-        $fields->addFieldToTab('Root.Main', OptionsetField::create('ContentBlock', false, $options, $checked));
+		return $fields;
+	}
 
-        $this->extend('updateCMSSelectionFields', $fields);
+	/**
+	 * @return DBField
+	 */
+	public function getThumbnail()
+	{
+		return DBField::create_field('HTMLText', sprintf('<img src="%s" height="20">', $this->getPreviewImagePath()));
+	}
 
-        return $fields;
-    }
+	/**
+	 * @return string
+	 */
+	protected function getPreviewImagePath()
+	{
+		$previewImagePath = $this->config()->get('preview');
 
-    public function providePermissions()
-    {
-        return [
-            'VIEW_CONTENT_BLOCKS' => [
-                'name' => 'View content blocks',
-                'help' => 'Allow viewing page content blocks',
-                'category' => 'Page - Content Blocks',
-                'sort' => 100
-            ],
-            'CREATE_CONTENT_BLOCKS' => [
-                'name' => 'Create content blocks',
-                'help' => 'Allow creating page content blocks',
-                'category' => 'Page - Content Blocks',
-                'sort' => 100
-            ],
-            'EDIT_CONTENT_BLOCKS' => [
-                'name' => 'Edit content blocks',
-                'help' => 'Allow editing page content blocks',
-                'category' => 'Page - Content Blocks',
-                'sort' => 100
-            ],
-            'DELETE_CONTENT_BLOCKS' => [
-                'name' => 'Delete content blocks',
-                'help' => 'Allow deleting page content blocks',
-                'category' => 'Page - Content Blocks',
-                'sort' => 100
-            ]
-        ];
-    }
+		$this->extend('updatePreviewImagePath', $previewImagePath);
 
-    public function canView($member = null, $context = []) 
-    {
-        return Permission::check('VIEW_CONTENT_BLOCKS', 'any', $member);
-    }
+		return $previewImagePath;
+	}
 
-    public function canCreate($member = null, $context = []) 
-    {
-        return Permission::check('CREATE_CONTENT_BLOCKS', 'any', $member);
-    }
+	public function getCMSFields()
+	{
+		$fields = parent::getCMSFields();
+		$fields->removeByName('Pages');
 
-    public function canEdit($member = null, $context = []) 
-    {
-        return Permission::check('EDIT_CONTENT_BLOCKS', 'any', $member);
-    }
+		if($this->getAction() == 'new') {
+			return $this->getCMSSelectionFields($fields);
+		} else {
+			$editor = GridFieldConfig_RelationEditor::create();
+			$grid = new GridField('Pages', 'Pages', $this->Pages(), $editor);
+			$grid->getConfig()
+				->removeComponentsByType(GridFieldAddNewButton::class);
+			$fields->addFieldToTab('Root.Pages', $grid);
+		}
+		return $fields;
+	}
 
-    public function canDelete($member = null, $context = []) 
-    {
-        return Permission::check('DELETE_CONTENT_BLOCKS', 'any', $member);
-    }
+	public function getTemplateHolder()
+	{
+		return $this->renderWith(['Block/ContentBlock_holder']);
+	}
+
+	public function getTemplate()
+	{
+		if($this->ClassName != ContentBlock::class) {
+			return $this->renderWith('Block/'.$this->ClassName);
+		}
+	}
+
+	public function getAction()
+	{
+		$path = explode('/', Controller::curr()->getRequest()->getURL());
+		return array_pop($path);
+	}
+
+	public function getBlockType()
+	{
+		$className = $this->owner->ClassName;
+		return $className::config()->get('title');
+	}
+
+	private function getCMSSelectionFields(FieldList $fields)
+	{
+		$fields->removeByName('Root');
+		// fields used in the inital selection request
+		$session = Controller::curr()->getRequest()->getSession();
+		$fields->push(HiddenField::create('BlockRelationID')->setValue($session->get('BlockRelationID')));
+		$fields->push(HiddenField::create('BlockRelationClass')->setValue($session->get('BlockRelationClass')));
+
+		// create the selection tab and options
+		$fields->push(TabSet::create('Root', Tab::create('Main')));
+
+		$rules = (array) Config::inst()->get(ContentBlock::class, 'restrict');
+
+		if(array_key_exists($session->get('BlockRelationClass'), $rules)) {
+			$classes = $rules[$session->get('BlockRelationClass')];
+		} else {
+			$classes = (array) Config::inst()->get(ContentBlock::class, 'blocks');
+		}
+		$options = [];
+		foreach($classes as $class) {
+			$block_inst = Injector::inst()->create($class);
+
+			$options[$class] = DBField::create_field('HTMLText', Controller::curr()
+				->customise([
+					'Preview'     => singleton($class)->getPreviewImagePath(),
+					'Title'       => $block_inst->i18n_singular_name(),
+					'Description' => $class::config()->get('description')
+				])
+				->renderWith('/Includes/ContentBlockOption')
+			);
+		}
+		$checked = key(array_slice($options, 0, 1, true));
+
+		$fields->addFieldToTab('Root.Main', OptionsetField::create('ContentBlock', false, $options, $checked));
+
+		$this->extend('updateCMSSelectionFields', $fields);
+
+		return $fields;
+	}
+
+	public function providePermissions()
+	{
+		return [
+			'VIEW_CONTENT_BLOCKS' => [
+				'name' => 'View content blocks',
+				'help' => 'Allow viewing page content blocks',
+				'category' => 'Page - Content Blocks',
+				'sort' => 100
+			],
+			'CREATE_CONTENT_BLOCKS' => [
+				'name' => 'Create content blocks',
+				'help' => 'Allow creating page content blocks',
+				'category' => 'Page - Content Blocks',
+				'sort' => 100
+			],
+			'EDIT_CONTENT_BLOCKS' => [
+				'name' => 'Edit content blocks',
+				'help' => 'Allow editing page content blocks',
+				'category' => 'Page - Content Blocks',
+				'sort' => 100
+			],
+			'DELETE_CONTENT_BLOCKS' => [
+				'name' => 'Delete content blocks',
+				'help' => 'Allow deleting page content blocks',
+				'category' => 'Page - Content Blocks',
+				'sort' => 100
+			]
+		];
+	}
+
+	public function canView($member = null, $context = [])
+	{
+		return Permission::check('VIEW_CONTENT_BLOCKS', 'any', $member);
+	}
+
+	public function canCreate($member = null, $context = [])
+	{
+		return Permission::check('CREATE_CONTENT_BLOCKS', 'any', $member);
+	}
+
+	public function canEdit($member = null, $context = [])
+	{
+		return Permission::check('EDIT_CONTENT_BLOCKS', 'any', $member);
+	}
+
+	public function canDelete($member = null, $context = [])
+	{
+		return Permission::check('DELETE_CONTENT_BLOCKS', 'any', $member);
+	}
 }
